332ebc37f4a1fddbd4ff4eafddde7e99349e74ad
Akash Pal <91068643+akashjpal@users.noreply.github.com>
2022-11-12 14:33:39 +0530

    Added files via upload

--- app.js
+++ app.js
@@ -1,39 +1,146 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname+"/date.js");
+const mongoose = require("mongoose");
+const _ = require("lodash");
 const app = express();
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
-var items = ["Awake","Learn","Fun"];
-var workItems = []
+mongoose.connect("mongodb+srv://akash:akash@cluster0.trdd6ez.mongodb.net/todolistDB");
+
+const itemSchema = {
+  name:String
+}
+
+const Item = mongoose.model("Item",itemSchema);
+
+const item1 = new Item({
+  name:"Welcome to to-do-list!"
+})
+const item2 = new Item({
+  name:"Hit the + button to add more items"
+})
+const item3 = new Item({
+  name:"Hit the delete button to remove items"
+})
+
+const defaultItem = [item1,item2,item3];
+
+const listSchema = {
+  name:String,
+  items : [itemSchema]
+}
+
+const List = mongoose.model("List",listSchema)
 
 app.get("/",function(req,res){
-  let day = date.getDate();
-  res.render('index', {kindOfDay : day,newListItems:items,listType:"To do List"});
+
+  Item.find({},function(err,Items){
+    if(Items.length === 0){
+      Item.insertMany(defaultItem,function(err){ 
+    if(err){
+      console.log(err);
+    }else{
+      console.log("Succesfully Inserted!!");
+    }
+  });
+  res.redirect("/");
+  }else{
+     res.render('index', {kindOfDay : "Today",newListItems:Items,listType:"To do List"});
+}
+})
 })
 
 app.post("/",function(req,res){
-  var input = req.body.newItem;
+  var inputName = req.body.newItem;
+  var listName = req.body.button;
+
   // console.log(req.body);
-  if(req.body.button === "Work"){
-    workItems.push(input);
-    res.redirect("/work");
+  const item = new Item({
+  name:inputName
+  });
+  
+
+  if(listName === "Today"){
+    item.save();
+    res.redirect("/");
   }else{
-  items.push(input);
-  res.redirect("/");
-}
+    List.findOne({name:listName},function(err,foundList){
+      foundList.items.push(item);
+      foundList.save();
+      res.redirect("/"+listName);
+    });
+  }
+//  res.redirect("/");
 })
 
-app.get("/work",function(req,res){
-  res.render('index', {kindOfDay : "Work List",newListItems:workItems,listType:"Work List"});
-})
+app.post("/delete",function(req,res){
+  // console.log(req.body.checkbox);
+  const item_id = req.body.checkbox;
+  const ilistName = req.body.DlistName;
+
+  if(ilistName === "Today"){
+  Item.findByIdAndRemove(item_id,function(err){
+    if(err){
+      console.log(err);
+    }else{
+      console.log("Deleted Successfully!!");
+    }
+  });
+  res.redirect("/");
+}
+else{
+  List.findOneAndUpdate({name:ilistName},{$pull:{items:{_id:item_id}}},function(err){
+    if(err){
+      console.log(err);
+    }else{
+      res.redirect("/"+ilistName);
+    }
+  });
+}
+});
+
+// app.get("/work",function(req,res){
+//   res.render('index', {kindOfDay : "Work List",newListItems:workItems,listType:"Work List"});
+// })
 
 app.get("/about",function(req,res){
   res.render('about',{listType:"About"});
 })
-app.listen(process.env.PORT || 3000,function(req,res){
-    console.log("Server has Started on port 3000!")
+
+app.get("/:customListname", function(req , res){
+  // console.log(req.params.customListname);
+  // const customListname = req.params.customListname;
+  const customListname = _.capitalize(req.params.customListname);
+
+ 
+  List.findOne({ name: customListname }, function (err,foundList) {
+    if(!err){
+      if(!foundList){
+        // Create a new list
+        const list = new List({
+        name:customListname,
+        items:defaultItem
+        });
+        list.save();
+        res.redirect("/"+customListname);
+      }
+      else{
+        res.render("index",{kindOfDay : foundList.name,newListItems:foundList.items,listType:"Work List"})
+      }
+    }
+  });
+
+
+});
+
+
+// app.get("/category/:work",function(req,res){
+//   console.log(req.params.work);
+//   res.render('index', {kindOfDay : req.params.work,newListItems:defaultItem,listType:"Work List"});
+// })
+app.listen(process.env.PORT || 2000,function(req,res){
+    console.log("Server has Started on port 2000!")
 })
